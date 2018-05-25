587dad89b8d6650bf84c602977de9243d78cf4b3
Shiny Alex <carvantes.alex@gmail.com>
2018-05-25 16:01:32 -0500

    Almos done html home

--- templates/home.php
+++ templates/home.php
@@ -3,7 +3,182 @@
     get_header();
 ?>
 
-section#
+<section id="home-banner">
+    <div class="left">
+        <div class="bluebox center">
+            <h1>PROTEGE TU CASA,<br>
+            PROTEGE TU NEGOCIO</h1>
+            <p class="white">
+                 
+                <b>STEELSHARP,</b> te ofrece
+                <b>Puertas y Ventanas Blindadas,</b>
+                hechas a medida para tu hogar, negocio y oficina
+            </p>
+        </div>
+        <div class="overlay">
+            <a href="#" class="btn-navy">ME INTERESA</a>
+        </div>
+    </div>
+</section>
+
+<section id="who">
+    <div class="flex">
+        <div class="col">
+            <img src="../img/home/CajaFuerteYdemas.png" alt="Caja fuerte e inmuebles de alta seguridad" id="cajas-fuertes">
+        </div>
+        <div class="col">
+            <h2>¿Quiénes <br><span class="pad-left dark-blue">Somos?</span></h2>
+            <p>
+                Somos una empresa mexicana con más de 25 años de 
+                <span class="dark-blue">experiencia </span>
+                en la fabricación de
+                <span class="dark-blue">soluciones de seguridad.</span>
+            </p>
+            <p>
+                Nos enfocamos en brindar 
+                <span class="navy">productos resistentes y duraderos</span>
+                , hechos con los mejores 
+                <span  class="navy">materiales certificados</span>
+                y la
+                <span  class="navy">mayor tecnología</span>
+            </p>
+            <p>
+                Ofrecemos productos 
+                <span class="sky"> diseñados a tus necesidades, con </span>
+                la
+                <span class="sky">asesoría de expertos</span>
+                capacitados.
+            </p>
+            
+        </div>
+    </div>
+</section>
+
+<section id="valores">
+    <div class="center dark-blue-back">
+        <h2>
+            <span class="heavy white"></span>
+        </h2>
+    </div>
+    <div class="flex">
+        <div class="col right">
+            <p>Veracidad ·</p>
+            <p>Responsabilidad ·</p>
+            <p>Honestidad · </p>
+            <p>Compromiso ·</p>
+        </div>
+        <div class="col left">
+            <p>· Confidencialidad</p>
+            <p>· Bienestar</p>
+            <p>· Orgullo</p>
+            <p>· Efectividad</p>
+        </div>
+    </div>
+</section>
+
+<section id="proposito">
+    <div class="flex">
+        <div class="col">
+            <h2>
+                Nuestro Propósito<br>
+                <span class="dark-blue pad-left">en
+                <span class="steelsharp">STEELSHARP</span>
+                </span>
+
+            </h2>
+            
+            <p class="border-left">
+                Brindarte tranquilidad, con soluciones de seguridad que resguarden tu patrimonio.
+            </p>
+        </div>
+        <div class="col">
+            <img src="../img/home/miss-flowers.jpg" alt="mujer alegre">
+        </div>
+    </div>
+</section>
+
+<section id="patrimonio">
+    <div class="center dark-blue-back">
+        <h2 class="white">Blindamos tu<br>
+            <span class="heavy">Patrimonio</span>
+        </h2>
+    </div>
+    <div class="flex">
+        <div class="col center">
+            <div class="svg"><img src="../img/icons/1.svg" alt="Equipo de computo"></div>
+            <p>
+                Mayor tecnología en el sistema de  combinaciones, con la certificación “UL de U.S.A.” (Underwriters Laboratorios), más sofisticado y difícil de descifrar. 
+            </p>
+        </div>
+        <div class="col center">
+            <div class="svg"><img src="../img/icons/2.svg" alt="Mecanismos"></div>
+            <p>
+                Mecanismos de combinación certificados, combinaciones mecánicas y digitales fáciles de programar.
+            </p>
+        </div>
+        <div class="col center">
+            <div class="svg"><img src="../img/icons/3.svg" alt="Herramientas"></div>
+            <p>
+                Nuestros productos son fáciles de instalar y ofrecemos servicio de instalación en algunas ciudades del territorio mexicano.
+            </p>
+        </div>
+    </div>
+    <div class="center"><img src="../img/home/line-h.png" alt="---------------------------------------"></div>
+    
+    
+    <div class="flex">
+        <div class="col center">
+            <div class="svg"><img src="../img/icons/4.svg" alt="Viga"></div>
+            <p>
+                Mayor durabilidad y resistencia en los materiales. 
+                Los productos son fabricados con  placas de acero tratado de alto calibre.
+            </p>
+        </div>
+        <div class="col center">
+            <div class="svg"><img src="../img/icons/5.svg" alt="Regla"></div>
+            <p>
+                Fabricamos nuestros productos con  medidas personalizadas de acuerdo a las necesidades del cliente.
+            </p>
+        </div>
+        <div class="col center">
+            <div class="svg"><img src="../img/icons/6.svg" alt="Envíos"></div>
+            <p>
+                Ventas y envíos a nivel nacional, con asesoría para la  instalación del producto.
+            </p>
+        </div>
+    </div>
+</section>
+
+<section id="soluciones">
+    <div class="center">
+        <h2>
+            <p>SOLUCIONES DE SEGURIDAD
+            <span class="heavy dark-blue">QUE TE OFRECEMOS</span></p>
+        </h2>
+    </div>
+    <p>
+        Para casas, oficinas, negocios y empresas, 
+        <b>STEELSHARP </b>
+         te ofrece soluciones de seguridad personalizada con la mayor tecnología y los mejores materiales en cajas fuertes, puertas y ventanas blindadas.
+    </p>
+    
+    <div class="flex">
+        <div class="col"><img src="../img/home/cajaFuerte.png" alt="caja fuerte"></div>
+        <div class="col"><img src="../img/home/PuertaBlindada.png" alt="Esclusa blindada"></div>
+        <div class="col"><img src="../img/home/VidrioBlindado.png" alt="Ventana blindada"></div>
+    </div>
+    <div class="flex">
+        <p>Cajas Fuertes</p>
+    </div>
+    <div class="flex">
+        <p>Ventanas</p>
+        <span class="dark-blue heavy">Blindadas</span>
+    </div>
+    <div class="flex">
+        <p>Ventanas</p>
+        <span class="dark-blue heavy">Blindadas</span>
+    </div>
+</section>
 
 <?php 
     get_footer();
